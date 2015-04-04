defd8935cabf89bb4aa737b1233a6ec706c739e4
Dan Lew <danlew42@gmail.com>
2015-04-04 07:55:38 -0500

    Pseudolocalization app

--- app/src/main/java/net/danlew/pseudolocalization/MainActivity.java
+++ app/src/main/java/net/danlew/pseudolocalization/MainActivity.java
@@ -1,9 +1,8 @@
 package net.danlew.pseudolocalization;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,27 +10,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        TextView paramTextView = (TextView) findViewById(R.id.param_text_view);
+        paramTextView.setText(getString(R.string.param_string, "Dan"));
     }
 }
